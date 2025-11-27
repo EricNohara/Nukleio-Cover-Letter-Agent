68b84fcd555f86bec9b395b243338ae74a767e7d
EricNohara <ernohara@bu.edu>
2025-11-27 15:37:53 -0600

    Add fix to test for first draft generation

--- src/tests/testFirstDraftAgent.ts
+++ src/tests/testFirstDraftAgent.ts
@@ -1,8 +1,6 @@
 import fs from "fs";
 import path from "path";
 import firstDraftAgent from "../agents/firstDraftAgent";
-import { ITheirStackJob } from "../interfaces/ITheirStackResponse";
-import { IUserInfo } from "../interfaces/IUserInfoResponse";
 import { WritingAnalysis } from "../utils/writing/writingSchema";
 import getOpenAIClient from "../utils/ai/getOpenAIClient";
 
@@ -14,15 +12,8 @@ function loadJSON<T>(file: string): T {
 
 async function main() {
   const openAI = getOpenAIClient();
-  const jobData = loadJSON<ITheirStackJob>("jobApiResponse.json");
-  const userData = loadJSON<IUserInfo>("userInfo.json");
   const writingAnalysis = loadJSON<WritingAnalysis>("writingAnalysis.json");
-  const draft = await firstDraftAgent(
-    openAI,
-    userData,
-    jobData,
-    writingAnalysis
-  );
+  const draft = await firstDraftAgent(openAI, "", writingAnalysis);
 
   console.log(draft);
 }
