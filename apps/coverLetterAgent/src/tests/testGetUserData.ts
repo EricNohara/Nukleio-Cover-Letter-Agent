9f81e5c14e723c34cab19401d283054d9e26b581
EricNohara <ernohara@bu.edu>
2026-04-02 23:22:29 -0400

    deleted cover letter tests

--- apps/coverLetterAgent/src/tests/testGetUserData.ts
+++
@@ -1,8 +0,0 @@
-import getUserData from "../utils/nukleio/getUserData";
-
-async function runTest() {
-  const data = await getUserData("d206d86e-88d3-4f9d-986c-40352f4e952f");
-  console.log(JSON.stringify(data, null, 2));
-}
-
-runTest();
